7b36c2ec481286123c8ef84abd9ce09b3fc69494
Alex Zvoleff <azvoleff@gmail.com>
2014-01-07 13:25:06 -0500

    Add data man pages.

--- R/lspathrow-package.R
+++ R/lspathrow-package.R
@@ -3,3 +3,21 @@
 #' @name lspathrow
 #' @docType package
 NULL
+#' Worldwide Reference System - 1 (WRS1) SpatialPolygonsDataFrame
+#' 
+#' Contains polygons of the area of each path and row for the ascending and 
+#' descending nodes of the WRS1 system.
+#'
+#' @docType data
+#' @name wrs1_asc_desc
+#' @seealso wrs2_asc_desc
+NULL
+#' Worldwide Reference System - 2 (WRS2) SpatialPolygonsDataFrame
+#' 
+#' Contains polygons of the area of each path and row for the ascending and 
+#' descending nodes of the WRS2 system.
+#'
+#' @docType data
+#' @name wrs2_asc_desc
+#' @seealso wrs1_asc_desc
+NULL
